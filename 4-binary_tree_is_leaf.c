c4baebe9ece5eaa086e4c33335d7b750b08bf232
20Emi <iop.emilysanchez1@gmail.com>
2023-05-12 11:25:59 -0700

    complete task

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,12 +5,7 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	binary_tree_t *leaf = NULL;
-
-	if (!node)
-		return (0);
-	
-	if (node != leaf)
+	if (node == NULL || node->right != NULL || node->left != NULL)
 		return (0);
 	
 	return (1);
