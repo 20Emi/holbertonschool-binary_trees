0c9f463e2a5850c538ee4b1bdf16f41a4124d1ba
20Emi <iop.emilysanchez1@gmail.com>
2023-05-12 09:36:05 -0700

    incomplete task

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * 
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	binary_tree_t *leaf = NULL;
+
+	if (!node)
+		return (0);
+	
+	if (node != leaf)
+		return (0);
+	
+	return (1);
+}
